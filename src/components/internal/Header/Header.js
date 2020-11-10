98b64fc6a7e6f02701c702e63acc5f1a18ea30ff
subbiah <subbiah@f22labs.com>
2020-11-10 21:35:13 +0530

    Reduce header spacing

--- src/components/internal/Header/Header.js
+++ src/components/internal/Header/Header.js
@@ -8,16 +8,16 @@ function Header() {
           <img src="/icons/logo.png" alt="lloyds" />
         </div>
         <div className="flex flex-row my-3 mb-0">
-          <div className="w-48 py-4 mx-6 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
+          <div className="w-48 py-4 mx-1 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
             Problem
           </div>
-          <div className="w-48 py-4 mx-6 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
+          <div className="w-48 py-4 mx-1 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
             Solution
           </div>
-          <div className="w-48 py-4 mx-6 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
+          <div className="w-48 py-4 mx-1 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
             Latest
           </div>
-          <div className="w-48 py-4 mx-6 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
+          <div className="w-48 py-4 mx-1 ml-0 h-10 cursor-pointer bg-gray-1000 text-white text-lg font-bold flex justify-center items-center">
             Act
           </div>
         </div>
