644ae56f37026f4bb2782cd4ebe8245f0fd87622
Shubham Ambastha <shubham.a@f22labs.com>
2020-11-24 15:30:18 +0530

    Members container wrapper created and imported in page

--- pages/members.js
+++ pages/members.js
@@ -1,12 +1,13 @@
 import Footer from "../src/components/internal/Footer";
 import Header from "../src/components/internal/Header";
+import MembersContainer from "../src/components/internal/MembersContainer";
 
 function Members() {
   return (
     <div className="bg-white lg:bg-gray-150">
       <Header />
-      <div className="h-screen bg-gray-900 flex justify-center pt-12 text-6xl text-white primary-extra-bold">
-        Members
+      <div className="bg-gray-900 flex justify-center pt-20 text-6xl text-white primary-extra-bold">
+        <MembersContainer />
       </div>
       <Footer />
     </div>
