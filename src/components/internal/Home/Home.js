ebe61c870756d9e8fbfb01f41dc55ddddb7421ae
subbiah <subbiah@f22labs.com>
2020-11-25 10:06:22 +0530

    Added tweets section

--- src/components/internal/Home/Home.js
+++ src/components/internal/Home/Home.js
@@ -8,16 +8,22 @@ import LatestContainer from "../LatestContainer";
 import SignUpContainer from "../SignUpContainer";
 import ActNowContainer from "../ActNowContainer/ActNowContainer";
 import Footer from "../Footer";
+import Head from "next/head";
+import TweetContainer from "../TweetContainer/TweetContainer";
 
 const Home = () => {
   return (
     <div className="fixed w-full h-full z-50 overflow-scroll">
+      <Head>
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+      </Head>
       <Header />
       <HeroContainer />
       <ProbSolutionContainer />
       <ProudBannerContainer />
       <LatestContainer />
       <SignUpContainer />
+      <TweetContainer />
       {/* <ActNowContainer /> */}
       <Footer />
     </div>
---
+++ src/components/internal/TweetContainer/TweetContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TweetContainer() {
+  return (
+    <section className="w-full py-16">
+      <div className="elfsight-app-38536a33-2fe7-48eb-bb3a-33b09c166c32"></div>
+    </section>
+  );
+}
+
+export default TweetContainer;
