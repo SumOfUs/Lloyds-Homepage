02b6949984d78be15f60eefbc8f21bceaa8a5fc2
subbiah <subbiah@f22labs.com>
2020-11-26 21:45:42 +0530

    Updated Solution subsection page

---
+++ pages/solution/what-others-are-doing.js
@@ -0,0 +1,18 @@
+import Footer from "../../src/components/internal/Footer";
+import Header from "../../src/components/internal/Header";
+import ImageCard from "../../src/components/internal/ImageCard/ImageCard";
+import Accordion from "../../src/components/internal/Accordion/Accordion";
+
+function OthersDoing() {
+  return (
+    <div className="w-full">
+      <Header />
+      <div className="bg-black mt-100 py-16 h-screen text-6xl text-white primary-black text-center">
+        Coming Soon ...
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default OthersDoing;
