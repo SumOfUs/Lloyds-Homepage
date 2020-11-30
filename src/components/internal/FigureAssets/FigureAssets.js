687663b232a67d64bea50d15cf51e75958351e2e
Shubham Ambastha <shubham.a@f22labs.com>
2020-11-30 13:21:06 +0530

    ui fixes

--- src/components/internal/FigureAssets/FigureAssets.js
+++ src/components/internal/FigureAssets/FigureAssets.js
@@ -3,36 +3,38 @@ import React from "react";
 function FigureAssets({ assets }) {
   const lastIdx = assets.length - 1;
   return (
-    <section className="w-full px-8 sm:px-20 py-16 bg-black text-white">
-      <div className="mb-8 sm:mb-12">
-        <h3 className="font-bold sm:text-5xl text-2xl mb-5 sm:mb-8 primary-black">
-          Figure on Assets
-        </h3>
-        <p className="sm:text-2xl text-base primary-semi-bold">
-          There are three links in the Chain of Security making upassets of
-          approx £82 billion (2019)
-        </p>
-      </div>
-      <div className="flex lg:flex-row flex-col w-full justify-between items-center">
-        {assets.map((asset, idx) => {
-          return (
-            <div
-              className={`max-w-sm w-full bg-gray-1000 lg:pt-16 px-8 items-center lg:items-start justify-center lg:justify-start  h-48 sm:h-280 flex flex-col lg:my-0 my-5 ${
-                idx !== lastIdx && "lg:mr-10"
-              }`}
-              key={Math.random()}
-            >
-              <h2 className="xl:text-6xl sm:text-4xl text-4xl leading-none font-bold primary-extra-bold">
-                {asset.figure}
-              </h2>
-              <p className="xl:text-3xl sm:text-2xl text-xl mt-2 primary-medium">
-                {asset.info}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+    <div className="bg-black">
+      <section className="py-10 sm:py-16 text-white max-w-1440 3xl:mx-auto 2xl:mx-16 mx-12">
+        <div className="mb-8 sm:mb-12">
+          <h3 className="font-bold sm:text-5xl text-2xl mb-5 sm:mb-8 primary-black">
+            Figure on Assets
+          </h3>
+          <p className="sm:text-2xl text-base primary-semi-bold">
+            There are three links in the Chain of Security making upassets of
+            approx £82 billion (2019)
+          </p>
+        </div>
+        <div className="flex lg:flex-row flex-col w-full justify-between items-center">
+          {assets.map((asset, idx) => {
+            return (
+              <div
+                className={`max-w-sm w-full bg-gray-1000 lg:pt-16 px-8 items-center lg:items-start justify-center lg:justify-start  h-48 sm:h-280 flex flex-col lg:my-0 my-5 ${
+                  idx !== lastIdx && "lg:mr-10"
+                }`}
+                key={Math.random()}
+              >
+                <h2 className="xl:text-6xl sm:text-4xl text-4xl leading-none font-bold primary-extra-bold">
+                  {asset.figure}
+                </h2>
+                <p className="xl:text-3xl sm:text-2xl text-xl mt-2 primary-medium">
+                  {asset.info}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/components/internal/InfoWithCta/InfoWithCta.js
+++ src/components/internal/InfoWithCta/InfoWithCta.js
@@ -3,17 +3,19 @@ import React from "react";
 function InfoWithCta({ heading, description, buttonText, isDark }) {
   return (
     <div
-      className={`sm:px-20 sm:py-16 p-8 ${
+      className={`${
         isDark ? "bg-gray-1500 text-white" : "bg-gray-200 text-gray-900"
       }`}
     >
-      <h3 className="primary-extra-bold sm:text-5xl text-2xl mb-5 sm:mb-10">
-        {heading}
-      </h3>
-      <p className="sm:text-4xl text-2xl primary-semi-bold">{description}</p>
-      <button className="mt-12 max-w-xs w-full py-6 bg-blue-900 text-white sm:text-2xl text-xl primary-bold">
-        {buttonText}
-      </button>
+      <div className="sm:py-16 py-8 max-w-1440 3xl:mx-auto 2xl:mx-16 mx-12">
+        <h3 className="primary-extra-bold sm:text-5xl text-2xl mb-5 sm:mb-10">
+          {heading}
+        </h3>
+        <p className="sm:text-4xl text-2xl primary-semi-bold">{description}</p>
+        <button className="mt-12 max-w-xs w-full py-6 bg-blue-900 text-white sm:text-2xl text-xl primary-bold">
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 }
